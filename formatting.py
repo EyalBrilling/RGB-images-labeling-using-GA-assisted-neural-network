a5ca1a9a67e51ff75476fee06af504d5cd2ed2db
Eyal <Eyalbrilling@gmail.com>
2022-08-16 17:39:51 +0300

    added formatting and neuralNetwork files
    formatting,py -
    csvToArray function: csv to npArray in readable format for the keras module. intToOnehot function: In addition a function for one-hots as helper
    
    * main() should be removed in some point
    
    neuralNetwork,py - the keras model building and compiltion

---
+++ formatting.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from keras.utils import to_categorical
+import pandas as pd
+
+
+def csvToArray(filePth):
+    csvDataFrame = pd.read_csv(filePth)
+    csvNumpyArray=csvDataFrame.to_numpy()
+    y_train_unflatten,x_train= np.split(csvNumpyArray,[1],axis=1)
+    y_train=y_train_unflatten.flatten()
+    return x_train,y_train
+
+def intToOnehot(labelArray):
+    oneHotList=[]
+    for i in range(len(labelArray)):
+        oneHotList.append(to_categorical(labelArray[i]-1, num_classes=10))
+    return np.array(oneHotList)
+
+def main():
+    x_train,y_train = csvToArray("validate.csv")
+    print(x_train.shape)
+    print(y_train.shape)
+    y_train_onehot = intToOnehot(y_train)
+    print(y_train_onehot)
+if __name__ == '__main__':
+    main()
---
+++ neuralNetwork.py
@@ -0,0 +1,46 @@
+from formatting import csvToArray,intToOnehot
+
+import matplotlib.pyplot as plt
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+
+
+
+POP_SIZE = 10
+EPOCH_NUM = 20
+
+
+def main():
+    x_train,y_train = csvToArray("train.csv")
+    x_test,y_test= csvToArray("validate.csv")
+
+    y_train = intToOnehot(y_train)
+    y_test = intToOnehot(y_test)
+
+    model = Sequential()
+    model.add(Flatten(input_shape=(3072,)))
+    model.add(Dense(32, activation='sigmoid'))
+    model.add(Dense(10, activation='softmax'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy', 
+              optimizer='adam',
+              metrics=['acc'])
+    # inisiate weights(chromosome) list
+    for chromosome in chromosomeList:
+        for i in range(EPOCH_NUM):
+            # set_weights of model to chromosome
+            model.train_on_batch(x_train, y_train)
+            # save weights after SGD
+    # test_on_batch for every chromosome
+    # save top half of chromosomes
+    # Crossover
+    # Mutation
+    # all over again
+    weights=model.get_weights()
+    #
+
+
+    #model.fit(x_train, y_train, epochs=10, validation_data=(x_test,y_test))
+
+if __name__ == '__main__':
+    main()
